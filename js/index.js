c71f5d1530f663bbb81bcf27a32b3eb3d36a0bf9
Jeremy Leader <jleader@alumni.caltech.edu>
2017-11-04 17:04:28 -0700

    Clean up indentation

--- js/index.js
+++ js/index.js
@@ -406,54 +406,54 @@ var ParticleExplosion = Class.extend({
     for (var i = this.particles.length - 1; i >= 0; i--) {
       var particle = this.particles[i];
       particle.moves++;
-            particle.x += particle.xunits;
-                  particle.y += particle.yunits + (particle.gravity * particle.moves);
-                        particle.life--;
+      particle.x += particle.xunits;
+      particle.y += particle.yunits + (particle.gravity * particle.moves);
+      particle.life--;
 
-                        if (particle.life <= 0 ) {
-                                if (this.particlePool.length < 100) {
-                                        this.particlePool.push(this.particles.splice(i,1));
-                                } else {
-                                        this.particles.splice(i,1);
-                                }
-                        } else {
-                                ctx.globalAlpha = (particle.life)/(particle.maxLife);
-                                ctx.fillStyle = particle.color;
-                                ctx.fillRect(particle.x, particle.y, particle.width, particle.height);
-                                ctx.globalAlpha = 1;
-                        }
+      if (particle.life <= 0 ) {
+        if (this.particlePool.length < 100) {
+                this.particlePool.push(this.particles.splice(i,1));
+        } else {
+                this.particles.splice(i,1);
+        }
+      } else {
+        ctx.globalAlpha = (particle.life)/(particle.maxLife);
+        ctx.fillStyle = particle.color;
+        ctx.fillRect(particle.x, particle.y, particle.width, particle.height);
+        ctx.globalAlpha = 1;
+      }
     }
   },
 
   createExplosion: function(x, y, color, number, width, height, spd, grav, lif) {
-  for (var i =0;i < number;i++) {
-                        var angle = Math.floor(Math.random()*360);
-                        var speed = Math.floor(Math.random()*spd/2) + spd;
-                        var life = Math.floor(Math.random()*lif)+lif/2;
-                        var radians = angle * Math.PI/ 180;
-                        var xunits = Math.cos(radians) * speed;
-                        var yunits = Math.sin(radians) * speed;
+    for (var i =0;i < number;i++) {
+      var angle = Math.floor(Math.random()*360);
+      var speed = Math.floor(Math.random()*spd/2) + spd;
+      var life = Math.floor(Math.random()*lif)+lif/2;
+      var radians = angle * Math.PI/ 180;
+      var xunits = Math.cos(radians) * speed;
+      var yunits = Math.sin(radians) * speed;
 
-                        if (this.particlePool.length > 0) {
-                                var tempParticle = this.particlePool.pop();
-                                tempParticle.x = x;
-                                tempParticle.y = y;
-                                tempParticle.xunits = xunits;
-                                tempParticle.yunits = yunits;
-                                tempParticle.life = life;
-                                tempParticle.color = color;
-                                tempParticle.width = width;
-                                tempParticle.height = height;
-                                tempParticle.gravity = grav;
-                                tempParticle.moves = 0;
-                                tempParticle.alpha = 1;
-                                tempParticle.maxLife = life;
-                                this.particles.push(tempParticle);
-                        } else {
-                                this.particles.push({x:x,y:y,xunits:xunits,yunits:yunits,life:life,color:color,width:width,height:height,gravity:grav,moves:0,alpha:1, maxLife:life});
-                        }
+      if (this.particlePool.length > 0) {
+        var tempParticle = this.particlePool.pop();
+        tempParticle.x = x;
+        tempParticle.y = y;
+        tempParticle.xunits = xunits;
+        tempParticle.yunits = yunits;
+        tempParticle.life = life;
+        tempParticle.color = color;
+        tempParticle.width = width;
+        tempParticle.height = height;
+        tempParticle.gravity = grav;
+        tempParticle.moves = 0;
+        tempParticle.alpha = 1;
+        tempParticle.maxLife = life;
+        this.particles.push(tempParticle);
+      } else {
+        this.particles.push({x:x,y:y,xunits:xunits,yunits:yunits,life:life,color:color,width:width,height:height,gravity:grav,moves:0,alpha:1, maxLife:life});
+      }
 
-                }
+    }
   }
 });
 
